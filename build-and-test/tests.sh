1a7f1106d0b8a73fc422492ef6657a7855950788
pavlosmcg <pavlosmcg@users.noreply.github.com>
2017-09-19 17:44:05 +0100

    Fix test script

--- build-and-test/tests.sh
+++ build-and-test/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd tools
-mono nunit/nuget.exe install NUnit
-mono nunit/nuget.exe install NUnit.Runners
+mono nuget/nuget.exe install NUnit
+mono nuget/nuget.exe install NUnit.Runners
 cd ..
 mono /build/tools/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe  /build/Assembler.Tests/bin/Debug/Assembler.Tests.dll
