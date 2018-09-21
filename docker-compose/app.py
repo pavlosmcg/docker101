ff9e585eefdfe80cbd10a9ef6e4da7f3c2c60716
Paul McGrath <paul@sciphus.com>
2018-09-21 21:28:44 +0100

    Final updates

--- docker-compose/app.py
+++ docker-compose/app.py
@@ -10,13 +10,9 @@ images = [
     "https://24.media.tumblr.com/tumblr_lstmtgYJVx1r4xjo2o1_500.jpg",
     "https://25.media.tumblr.com/tumblr_lyxvheqwhZ1r6b7kmo1_500.jpg",
     "https://25.media.tumblr.com/tumblr_m11oqlcb8s1r9qf7so1_500.jpg",
-    "https://24.media.tumblr.com/tumblr_m25zjtZy5x1qabm53o1_500.jpg",
     "https://24.media.tumblr.com/qgIb8tERipvdnd9870HESxL3o1_500.jpg",
     "https://25.media.tumblr.com/tumblr_lparluSFjh1qij6yko1_500.jpg",
-    "https://25.media.tumblr.com/tumblr_ltmntg3gCR1qcmkeso1_500.jpg",
     "https://28.media.tumblr.com/tumblr_lh6z4gpRNB1qfyzelo1_500.jpg",
-    "https://25.media.tumblr.com/RKwjoIoFXlyx98elpml4ZY98o1_400.jpg",
-    "https://26.media.tumblr.com/tumblr_m35gs01pWX1qhwmnpo1_500.jpg",
     "https://30.media.tumblr.com/tumblr_m3glwlj6J71r73wdao1_400.jpg",
 ]
 
