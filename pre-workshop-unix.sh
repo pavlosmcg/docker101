02015ef1025de54f2aa0e54a3ae210c40461a870
Paul McGrath <paul@sciphus.com>
2018-11-02 14:39:38 +0000

    Add pre-workshop scripts

---
+++ pre-workshop-unix.sh
@@ -0,0 +1,9 @@
+docker pull alpine
+docker pull nginx
+docker pull dockersamples/static-site
+docker pull python:3.4-alpine
+docker pull redis:alpine
+docker pull mono
+docker pull jetbrains/teamcity-server
+docker pull jetbrains/teamcity-agent
+docker run alpine echo "Ready to learn docker"
