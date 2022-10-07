ea42bcfaf3fba78c9d812b0cdd7d1431e3b06d05
pavlosmcg <pavlosmcg@users.noreply.github.com>
2022-10-07 16:37:20 +1300

    This will work forever

--- build-and-test/tests.sh
+++ build-and-test/tests.sh
@@ -4,4 +4,4 @@ cd tools
 mono nuget/nuget.exe install NUnit
 mono nuget/nuget.exe install NUnit.Runners
 cd ..
-mono /build/tools/NUnit.ConsoleRunner.3.12.0/tools/nunit3-console.exe  /build/Assembler.Tests/bin/Debug/Assembler.Tests.dll
+mono /build/tools/NUnit.ConsoleRunner.*/tools/nunit3-console.exe  /build/Assembler.Tests/bin/Debug/Assembler.Tests.dll
